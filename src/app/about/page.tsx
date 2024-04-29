8c76389f1a18904a72dcd22e7033796c4e6f63d1
liyunfei22 <liyunfei02@daojia-inc.com>
2024-04-29 23:17:34 +0800

    feat: about the pc

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,51 +10,59 @@ const plus_Jakarta_Sans = Plus_Jakarta_Sans({
 export default function About() {
   return (
     <main className="">
-      <section className="p-4">
-        <div className="flex justify-center">
-          <Image src="/Barber.png" width={143} height={160} alt="shops" />
+      <section className="p-4 lg:flex">
+        <div className="flex justify-center lg:shrink-0	">
+          <div className="lg:w-[631px] lg:h-[631px] overflow-hidden border hidden lg:block">
+            <Image src="/Barber-l.png" width={631} height={631} alt="shops" />
+          </div>
+          <div className="lg:hidden">
+            <Image src="/Barber.png" width={143} height={160} alt="shops" />
+          </div>
         </div>
-        <div className="font-bold text-xl py-4 text-center">About Us</div>
-        <div className="font-bold text-base pb-2 text-center">
-          PT MONGOL BALINDO VALAS
+        <div>
+          <div className="font-bold text-xl py-4 text-center lg:text-left lg:text-5xl lg:py-10">About Us</div>
+          <div className="font-bold text-base pb-2 text-center lg:text-left lg:text-xl lg:pb-4">
+            PT MONGOL BALINDO VALAS
+          </div>
+          <p
+            className={`${plus_Jakarta_Sans.className} text-sm indent-4 font-normal	pb-4 text-center lg:text-left lg:text-xl`}
+          >
+            At BitExchange, we pride ourselves on being a leading provider of
+            currency exchange and digital asset solutions. BitExchange
+            commitment to excellence and innovation, establishing as a trusted
+            partner for individuals and businesses alike.{" "}
+          </p>
+          <p
+            className={`${plus_Jakarta_Sans.className} text-sm indent-4 font-normal	pb-4 text-center lg:text-left lg:text-xl`}
+          >
+            Our extensive range of services includes the exchange of over 13
+            currencies from around the globe, ensuring that our customers can
+            easily access the funds they need wherever they are. In addition, we
+            offer seamless support for digital assets, providing our clients
+            with the flexibility and convenience of trading their cryptocurrency
+            holdings alongside traditional currencies.{" "}
+          </p>
+          <p
+            className={`${plus_Jakarta_Sans.className} text-sm indent-4 font-normal	pb-4 text-center lg:text-left lg:text-xl`}
+          >
+            Our dedicated team of professionals is passionate about delivering
+            superior customer service and creating value for our clients.
+            Whether you're a seasoned traveler seeking competitive exchange
+            rates or an investor looking to diversify your portfolio, you can
+            count on BitExchange for reliable, efficient, and secure financial
+            solutions.
+          </p>
         </div>
-        <p
-          className={`${plus_Jakarta_Sans.className} text-sm indent-4 font-normal	pb-4 text-center`}
-        >
-          At BitExchange, we pride ourselves on being a leading provider of
-          currency exchange and digital asset solutions. BitExchange commitment
-          to excellence and innovation, establishing as a trusted partner for
-          individuals and businesses alike.{" "}
-        </p>
-        <p
-          className={`${plus_Jakarta_Sans.className} text-sm indent-4 font-normal	pb-4 text-center`}
-        >
-          Our extensive range of services includes the exchange of over 13
-          currencies from around the globe, ensuring that our customers can
-          easily access the funds they need wherever they are. In addition, we
-          offer seamless support for digital assets, providing our clients with
-          the flexibility and convenience of trading their cryptocurrency
-          holdings alongside traditional currencies.{" "}
-        </p>
-        <p
-          className={`${plus_Jakarta_Sans.className} text-sm indent-4 font-normal	pb-4 text-center`}
-        >
-          Our dedicated team of professionals is passionate about delivering
-          superior customer service and creating value for our clients. Whether
-          you're a seasoned traveler seeking competitive exchange rates or an
-          investor looking to diversify your portfolio, you can count on
-          BitExchange for reliable, efficient, and secure financial solutions.
-        </p>
       </section>
-      <div className="w-full h-64	my-4">
+      <div className="w-full h-64	my-4 lg:h-[800px]">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15774.008233309467!2d115.1720793!3d-8.7387406!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2456cc382b135%3A0x20473c679f3048a4!2sBitExchange%20Money%20Changer%20(PT.%20Mongol%20Balindo%20Valas)!5e0!3m2!1sen!2s!4v1714286231984!5m2!1sen!2s"
-          className="border-0 w-full h-64"
+          className="border-0 w-full h-full"
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
-      <div>
+      <div className="lg:hidden">
         <div className="text-xl flex items-center justify-center font-semibold	text-center pb-4">
           <MapIcon />
           <div className="w-2"></div>BitExchange Store
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,14 @@ import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
 import LogInIcon from "@/components/icons/Logo";
+import LogInIconLarge from "@/components/icons/Logo-large";
 import MenuIcon from "@/components/icons/Menu";
 import LogInIcon2 from "@/components/icons/Logo2";
 import Facebook from "@/components/icons/Facebook";
 import Instagram from "@/components/icons/Instagram";
 import Linkedin from "@/components/icons/Linkedin";
 import Youtobe from "@/components/icons/Youtobe";
+import Link from 'next/link'
 
 const inter = Lato({
   subsets: ["latin"],
@@ -27,24 +29,36 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav className="w-full h-18 border-b border-black	 flex justify-between items-center px-4">
-          <LogInIcon/>
-          <MenuIcon/>
+        <nav className="w-full h-18 border-b lg:h-[120px] border-black	 flex justify-between items-center px-4">
+          <div className="lg:hidden"><LogInIcon/></div>
+          <div className="hidden lg:block lg:ml-20"><LogInIconLarge/></div>
+          <div>
+            <div className="lg:hidden"><MenuIcon/></div>
+            <div className="hidden lg:flex text-2xl color-black ">
+              <Link className="mr-8" href="/">Home</Link>
+              <Link className="mr-8" href="/about">About Us</Link>
+              <div className="mr-8">Contact Us</div>
+            </div>
+          </div>
         </nav>
         <main>{children}</main>
         <footer className="flex flex-col items-center py-6">
           <div className="pb-5">
             <LogInIcon2/>
           </div>
+          <div className="lg:flex lg:px-20 lg:justify-between lg:w-full">
           <div className="text-xs	text-black-100 pb-5">© 2024 BitExchange. All Rights Reserved</div>
-          <div className="flex items-center pb-5">
+          <div className="flex items-center pb-5 justify-center">
             <div className="mr-6"><Facebook></Facebook></div>
             <div className="mr-6"><Instagram></Instagram></div>
             <div className="mr-6"><Linkedin></Linkedin></div>
-            <div className="mr-6"><Youtobe></Youtobe></div>
+            <div className=""><Youtobe></Youtobe></div>
           </div>
-          <div className="font-bold	text-base	pb-5">Privacy Policy</div>
+          <div className="flex flex-col items-center lg:flex-row">
+          <div className="font-bold	text-base	pb-5 lg:mr-12">Privacy Policy</div>
           <div className="font-bold	text-base pb-5">Terms of Services</div>
+          </div>
+          </div>
         </footer>
       </body>
     </html>
