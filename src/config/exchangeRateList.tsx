aad5536255cf64267bff94e1409a6486fec47992
liyunfei22 <liyunfei02@daojia-inc.com>
2024-04-30 14:40:23 +0800

    feat: the icon

--- src/config/exchangeRateList.tsx
+++ src/config/exchangeRateList.tsx
@@ -1,8 +1,11 @@
 import GBPIcon from "@/components/flag/GBP";
 import USDIcon from "@/components/flag/USD";
+import AUDIcon from "@/components/flag/AUD";
 
 const exchangeRateList = [
   { value: "GBP", rate: 1.1, icon: <GBPIcon /> },
+  { value: "USD", rate: 1.2, icon: <USDIcon /> },
+  { value: "AUD", rate: 1.2, icon: <AUDIcon /> },
   { value: "USD", rate: 1.2, icon: <USDIcon /> },
 ];
 
